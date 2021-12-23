49cd0fad895d44e3f87ce1ef05e138ab173eb4c9
Arun <arunveebhuti123@gmail.com>
2021-12-23 21:41:30 +0530

    spring initialize

---
+++ src/test/java/com/contactsApplication/contacts/ContactsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.contactsApplication.contacts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
